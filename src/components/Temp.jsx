6546fcb026cae407c1c9f9214afa28f6ca63ca72
Ankit Kumar <ak9664773@gmail.com>
2023-02-13 01:16:57 +0530

    Applied API & fetching data

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import React , {useState , useEffect} from 'react'
 import "./Temp.css"
 
 
@@ -6,7 +6,48 @@ import "./Temp.css"
 
 const Temp = () => {
 
-    const [serachValue, setserachValue] = useState('');
+    const [serachValue, setserachValue] = useState('delhi');
+    const [tempInfo, settempInfo] = useState({})
+
+    const getWeatherInfo=async()=>{
+        try{
+            let url=`https://api.openweathermap.org/data/2.5/weather?q=${serachValue}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`; 
+
+            const res=await fetch(url);
+            const data=await res.json();
+            const {temp,pressure,humidity}=data.main;
+            const {main:weathermood}=data.weather[0];
+            const {name}=data;
+            const {speed}=data.wind;
+            const {country,sunset}=data.sys;
+            
+            const weatherInfo={
+                temp,
+                pressure,
+                humidity,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset
+            };
+
+            settempInfo(weatherInfo);
+            
+
+        }
+        catch(error){
+            console.log("Error - Unable to Fetch API")
+        }
+    }
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, [])
+    
+    let sec=tempInfo.sunset;
+    let date=new Date(sec*1000);
+    let timeStr=`${date.getHours()}:${date.getMinutes()}`
 
     return (
         <>
@@ -14,7 +55,7 @@ const Temp = () => {
 
                 <div className="box1">
                     <input type="search" placeholder='Search here...' value={serachValue} onChange={(e)=>{setserachValue(e.target.value)}}/>
-                    <button>Search</button>
+                    <button onClick={getWeatherInfo}>Search</button>
                 </div>
 
                 <div className="box2">
@@ -23,10 +64,10 @@ const Temp = () => {
                     </div>
                     <div className="section2">
                         <div className="black">
-                            <div className='black1'>28.57&#176;</div>
+                            <div className='black1'>{tempInfo.temp}&#176;</div>
                             <div className='black2'>
-                                <div className='weather'>CLOUDS</div>
-                                <div>Tokyo,Jp</div>
+                                <div className='weather'>{tempInfo.weathermood}</div>
+                                <div>{tempInfo.name},{tempInfo.country}</div>
                             </div>
                         </div>
                         <div className="blue">
@@ -37,31 +78,31 @@ const Temp = () => {
                     </div>
                     <div className="box3">
                         <div className="data">
-                            <i className="wi wi-day-thunderstorm" />
+                            <i className="wi wi-sunset" />
                             <div className="readings">
-                                <div className="stime">15:26 PM</div>
+                                <div className="stime">{timeStr}</div>
+                                <div className="work">Sunset</div>
+                            </div>
+                        </div>
+                        <div className="data">
+                            <i className="wi wi-humidity" />
+                            <div className="readings">
+                                <div className="stime">{tempInfo.humidity}</div>
                                 <div className="work">Humidity</div>
                             </div>
                         </div>
                         <div className="data">
-                            <i className="wi wi-day-thunderstorm" />
+                            <i className="wi wi-barometer" />
                             <div className="readings">
-                                <div className="stime">15:26 PM</div>
-                                <div className="work">Humidity</div>
+                                <div className="stime">{tempInfo.pressure}</div>
+                                <div className="work">Pressure</div>
                             </div>
                         </div>
                         <div className="data">
-                            <i className="wi wi-day-thunderstorm" />
+                            <i className="wi wi-strong-wind" />
                             <div className="readings">
-                                <div className="stime">15:26 PM</div>
-                                <div className="work">Humidity</div>
-                            </div>
-                        </div>
-                        <div className="data">
-                            <i className="wi wi-day-thunderstorm" />
-                            <div className="readings">
-                                <div className="stime">15:26 PM</div>
-                                <div className="work">Humidity</div>
+                                <div className="stime">{tempInfo.speed}</div>
+                                <div className="work">Speed</div>
                             </div>
                         </div>
                     </div>
